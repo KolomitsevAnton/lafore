060185e671eec203408d677a6f1986dbc72d17aa
Коломыцев Антон Владимирович <kolant@macbook-pro.local>
2018-02-05 01:58:59 +0300

    Initial Commit

---
+++ lafore/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lafore
+//
+//  Created by Коломыцев Антон Владимирович on 05.02.2018.
+//  Copyright © 2018 Коломыцев Антон Владимирович. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
